5293e14a0b2ba13fc53ad6023806f0fff3b14109
Varun Sukheja <varun.sukheja@myglamm.com>
2020-03-02 00:33:23 +0530

    completed product details page

--- src/components/ProductThumbnail/ProductThumbnail.js
+++ src/components/ProductThumbnail/ProductThumbnail.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './ProductThumbnail.scss';
 
 const ProductThumbnail = props => {
-  console.log(props);
   return (
     <article className="product-thumbnail-container">
-      <img src={props.product.imageUrl} />
+      <img src={props.product.imageUrl} alt="product image" />
       <div className="product-name">{props.product.name}</div>
       <div>
-        <button>See Details</button>
+        <Link to={'/product/' + props.product.id}>
+          <button>See Details</button>
+        </Link>
       </div>
     </article>
   );
